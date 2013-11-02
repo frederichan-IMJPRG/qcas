731ce7febdc22a072bb395c41fa5549a090771c1
Han Frederic <han@math.jussieu.fr>
2013-11-02 18:49:31 +0100

    trivial

--- qt/gui/CentralTabWidget.cpp
+++ qt/gui/CentralTabWidget.cpp
@@ -123,7 +123,7 @@ void MainTabWidget::closeTab(int id){
 
 }
 void MainTabWidget::insertG2dSheet(int i){
-    qDebug()<<"i="<<i;
+    //qDebug()<<"i="<<i;
     if((i<count())&&(i>=0)){
     this->insertTab(i,new GraphWidget(mainWindow->getContext(),true,mainWindow),QIcon(":/images/g2d.png"),tr("Feuille n°")+QString::number(i+1));
     this->setCurrentIndex(i);
